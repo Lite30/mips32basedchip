608520a0fe62d3bec1a1a5520d1c8df8cfba606d
Kent Tolo <119600055+KentTolo@users.noreply.github.com>
2024-01-04 01:33:06 +0200

    Update assembler3.cpp

--- assembler2.cpp
+++ assembler2.cpp
@@ -3,16 +3,17 @@
 #include<sstream>
 #include<string>
 #include<bitset>
-  
-
 
 using namespace std;
+
+// Structure to represent MIPS opcodes
 struct OPCODE
 {
     string opcodeStr;
     bitset<6> opcodeInt;
 };
 
+// Structure to represent MIPS registers
 struct RegProfile  
 {
     string registers; // format: $r1, $s2, $t2
@@ -21,15 +22,18 @@ struct RegProfile
 
 int main()
 {
+    // Initializing MIPS opcodes		
     OPCODE ADD { "add", bitset<6>(0b000000) };
     OPCODE DIV { "div", bitset<6>(0b000001) };
     OPCODE BRANCH { "branch", bitset<6>(0b000100) };
     OPCODE JUMP { "jump", bitset<6>(0b001000) };
     OPCODE LOAD { "load", bitset<6>(0b010000) };
     OPCODE AND { "and", bitset<6>(0b000010) };
-  
+
+    // Creating an array of opcodes	
     OPCODE arrayOp[6] = { ADD, DIV, BRANCH, JUMP, LOAD, AND };
 
+    // Initializing MIPS registers	
     RegProfile t0 { "$t0,", bitset<5>(0b00001) };
     RegProfile t1 { "$t1,", bitset<5>(0b00010) };
     RegProfile t2 { "$t2,", bitset<5>(0b00011) };
@@ -45,10 +49,11 @@ int main()
     RegProfile a2 { "$a2,", bitset<5>(0b01110) };
     RegProfile a3 { "$a3,", bitset<5>(0b01110) };
     RegProfile zero { "$zero,", bitset<5>(0b01111)};
-    
+
+	
+    // Creating an array of registers
     RegProfile arrayReg[14] = { t0, t1, t2, t3, s0, s1, s2 , s3 , v0, v1, a0, a1, a3, zero}; 
   
-  	/////
     ifstream in_file("mipsinstruction.txt"); // read from file
 
     if (!in_file.is_open())
@@ -66,49 +71,43 @@ int main()
         string command, operand1, operand2, operand3;
         bitset<6> temp0;
         bitset<5> temp1, temp2, temp3;
+	
         if (getline(iss, command, ' ') && getline(iss, operand1, ' ') && getline(iss, operand2, ' ') && getline(iss, operand3))
         {
-            // do something
+            // Find the corresponding opcode for the command
           	for(int i = 0; i< 6; i++)
             {
             	if(command == arrayOp[i].opcodeStr)
               {
                 temp0 = arrayOp[i].opcodeInt;
-							}
-              
-              
+	      }
+		    
             }
           	for(int l = 0; l< 14; l++)
             {
             	if(operand1 == arrayReg[l].registers)
               {
                  temp1 = arrayReg[l].regAddress;
-							}
+	      }
             }
-          
-          
           	for(int r = 0; r< 14; r++)
             {
             	if(operand2 == arrayReg[r].registers)
               {
                 temp2= arrayReg[r].regAddress;
-							}
+	      }
             }
-          
-          
           	for(int t = 0; t< 14; t++)
             {
             	if(operand3 == arrayReg[t].registers)
               {
                 temp3 = arrayReg[t].regAddress;
-							}
+	      }
             }
 
             cout << " opcode "<<  " rs \t" << " rt "<<"\trd "<<endl;
             cout << temp0 << " | "<< temp1 << " | "<<temp2 << " | "<<temp3 <<endl;
           	
-
-           // std::cout << "Command: " << command << ", Operand1: " << operand1 << ", Operand2: " << operand2 << ", Operand3: " << operand3 << std::endl;
         }
         else
         {
